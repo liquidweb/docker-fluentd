db4dcc1cc21561e13f2cc5e556699f0d073ef847
Dan Burke <dburke@addictmud.org>
2015-08-27 10:05:04 -0400

    Remove copy/paste crap from run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-ADD fluent.conf /etc/fluent/
-ADD fluent-kubernetes.conf /etc/fluent/
-
-ENTRYPOINT ["/usr/local/bin/fluentd", "-c", "/etc/fluent/fluent.conf"]
-
-
 if [ ! "${KUBERNETES_SERVICE_HOST}" ]; then
     /usr/local/bin/fluentd -c /etc/fluent/fluent-kubernetes.conf
 else
