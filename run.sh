e5ce7062bba03fd295f0a19557e9b17d519cc92e
Dan Burke <dburke@addictmud.org>
2015-08-27 10:31:29 -0400

    logic swap checking for kubernetes

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! "${KUBERNETES_SERVICE_HOST}" ]; then
-    /usr/local/bin/fluentd -c /etc/fluent/fluent-kubernetes.conf
-else
     /usr/local/bin/fluentd -c /etc/fluent/fluent.conf
+else
+    /usr/local/bin/fluentd -c /etc/fluent/fluent-kubernetes.conf
 fi
 
